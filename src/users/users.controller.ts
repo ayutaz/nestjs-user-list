f3c99cc9c0094288478f5910a13eab8c5c791e78
ayutaz <ka1357amnbpdr@gmail.com>
2022-04-17 03:37:26 +0900

    feat: user management api(add,getAll)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  findAll(): User[] {
+    return this.userService.findAll();
+  }
+
+  @Post()
+  create(@Body(ValidationPipe) user: User): void {
+    return this.userService.create(user);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  users: User[] = [];
+
+  create(user: User) {
+    this.users.push(user);
+  }
+
+  findAll() {
+    return this.users;
+  }
+}
